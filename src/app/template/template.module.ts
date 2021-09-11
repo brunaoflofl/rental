923cb283e7b15607f18a7dd3cb9fe119e18baada
Bruna Lima <t_brunao@hapvida.com.br>
2021-09-10 23:34:02 -0300

    form, card e footer

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,20 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarrocelComponent } from './carrocel/carrocel.component';
+import { FormComponent } from './form/form.component';
+import { CardsComponent } from './cards/cards.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    CarrocelComponent
+    CarrocelComponent,
+    FormComponent,
+    CardsComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     NavbarComponent,
-    CarrocelComponent
+    CarrocelComponent,
+    FormComponent,
+    CardsComponent,
+    FooterComponent
   ]
 })
 export class TemplateModule { }
